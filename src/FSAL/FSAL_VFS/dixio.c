2c412bd9ff35cb837a1b5e136f7e9b9774f25c96
Kelong Wang <kelong.wang@stonybrook.edu>
2014-08-14 16:30:06 -0400

    use io_extention to pass PI (for PI-patchset-v2)
    commit to this branch for archive

--- src/FSAL/FSAL_VFS/dixio.c
+++ src/FSAL/FSAL_VFS/dixio.c
@@ -13,15 +13,54 @@
 #define GENERATE_APP	(4)
 #define GENERATE_ALL	(7)
 
-#define NR_IOS	(1)
-
 #define MAX_AIO_EVENTS 2
-
-/* 8 bytes protection information. */
-#define PROT_INFO_SIZE 8
 
 #define PAGESIZE 4096
 #define PAGESHIFT 12
+
+/* Stuff that should go in libaio.h */
+#define IO_EXT_INVALID	(0)
+#define	IO_EXT_PI	(1)	/* protection info attached */
+
+#define IOCB_FLAG_EXTENSIONS	(1 << 1)
+
+#include <linux/fs.h>	/* get __u64, __u32 */
+struct io_extension {
+	__u64 ie_size;
+	__u64 ie_has;
+
+	/* PI stuff */
+	__u64 ie_pi_buf;
+	__u32 ie_pi_buflen;
+	__u32 ie_pi_ret;
+	__u32 ie_pi_flags;
+};
+
+static void io_prep_extensions(struct iocb *iocb, struct io_extension *ext,
+			       unsigned int nr)
+{
+	iocb->u.c.flags |= IOCB_FLAG_EXTENSIONS;
+	iocb->u.c.__pad3 = (long long)ext;
+}
+
+static void io_prep_extension(struct io_extension *ext)
+{
+	memset(ext, 0, sizeof(struct io_extension));
+	ext->ie_size = sizeof(*ext);
+}
+
+static void io_prep_extension_pi(struct io_extension *ext, void *buf,
+				 unsigned int buflen, unsigned int flags)
+{
+	ext->ie_has |= IO_EXT_PI;
+	ext->ie_pi_buf = (__u64)buf;
+	ext->ie_pi_buflen = buflen;
+	ext->ie_pi_flags = flags;
+}
+/* End stuff for libaio.h */
+
+#define READ	0
+#define WRITE	1
 
 #ifdef COMPONENT_FSAL
 #define DIX_ERR(msg, args...) LogError(COMPONENT_FSAL, msg, ##args)
@@ -30,21 +69,29 @@
 #endif
 
 static inline size_t page_roundup(size_t size) {
-	return (size + PAGESIZE - 1) >> PAGESHIFT;
+	return ((size + PAGESIZE - 1) >> PAGESHIFT) * PAGESIZE;
 }
 
 static inline int page_aligned(void *buf) {
 	return !((unsigned long)buf & (PAGESIZE - 1));
 }
 
-static ssize_t __do_dixio(int fd, off_t offset, int iocmd,
-			  const struct iovec *iov, int iovcnt)
+static ssize_t __do_dixio(int fd, void *buf, void *pbuf, size_t count,
+			  off_t offset, int rw)
 {
 	int ret;
+	struct iovec iov[1];
 	struct iocb cb = {0};
 	struct iocb *iocbps[1] = {&cb};
 	struct io_event events[1];
+	struct io_extension iocb_ext[1];
 	io_context_t ioctx;
+
+	assert(page_aligned(buf));
+	assert(page_aligned(pbuf));
+	assert(page_aligned((void *)count));
+	assert(page_aligned((void *)offset));
+	assert(rw == READ || rw == WRITE);
 
 	if (io_queue_init(MAX_AIO_EVENTS, &ioctx)) {
 		DIX_ERR("io_queue_init");
@@ -52,12 +99,19 @@ static ssize_t __do_dixio(int fd, off_t offset, int iocmd,
 		return -1;
 	}
 
-	cb.aio_fildes = fd;
-	cb.aio_lio_opcode = iocmd;
-	cb.aio_reqprio = 0;
-	cb.u.c.buf = (void *)iov;
-	cb.u.c.nbytes = iovcnt;
-	cb.u.c.offset = offset;
+	iov[0].iov_base = buf;
+	iov[0].iov_len = count;
+	if (rw == WRITE)
+		io_prep_pwritev(&cb, fd, iov, 1, offset);
+	else
+		io_prep_preadv(&cb, fd, iov, 1, offset);
+
+	if (pbuf) {
+		io_prep_extension(iocb_ext);
+		io_prep_extension_pi(iocb_ext, pbuf, get_pi_size(count),
+				     rw == WRITE ? GENERATE_GUARD : 0);
+		io_prep_extensions(&cb, iocb_ext, 1);
+	}
 
 	ret = io_submit(ioctx, 1, iocbps);
 	if (ret < 0) {
@@ -73,59 +127,41 @@ static ssize_t __do_dixio(int fd, off_t offset, int iocmd,
 
 	if ((ret = (signed)events[0].res) < 0) {
 		errno = -ret;
-		fprintf(stderr, "do_dixio %d failed: %d\n", iocmd, errno);
+		fprintf(stderr, "do_dixio(%d) failed: %d\n", rw, errno);
 		return ret;
 	}
 
-	io_queue_release(ioctx);
+	if (io_queue_release(ioctx))
+		fprintf(stderr, "io_queue_release failed\n");
 
 	return ret;
 }
 
 
-static void setup_iov(struct iovec *iov, void *buf, void *prot_buf,
-		      size_t count, off_t offset)
+static ssize_t do_dixio(int fd, void *buf, void *prot_buf, size_t count,
+			off_t offset, int rw)
 {
-	assert(page_aligned(buf));
-	assert(page_aligned(prot_buf));
-	assert(page_aligned((void *)count));
-	assert(page_aligned((void *)offset));
-
-	iov[0].iov_base = buf;
-	iov[0].iov_len = count;
-	iov[1].iov_base = prot_buf;
-	iov[1].iov_len = ((count >> 9) + 1) * PROT_INFO_SIZE;
-}
-
-
-ssize_t do_dixio(int fd, void *buf, void *prot_buf, size_t count, off_t offset,
-		 int iocmd)
-{
-	struct iovec iov[2];
-	int iovcnt = prot_buf ? 2 : 1;
 	void *pbuf = prot_buf;
 	ssize_t ret = 0;
 	size_t pi_size = 0;
 
 	if (prot_buf && !page_aligned(prot_buf)) {
 		pi_size = get_pi_size(count);
-		pbuf = gsh_malloc_aligned(PAGESIZE, page_roundup(pi_size));
+		pbuf = gsh_malloc_aligned(PAGESIZE, pi_size);
 		if (!pbuf) {
 			DIX_ERR("could not alloc memory for prot_buf");
 			errno = ENOMEM;
 			return -1;
 		}
 
-		if (iocmd == IOCB_CMD_PWRITEV || iocmd == IOCB_CMD_PWRITEVM)
+		if (rw == WRITE)
 			memcpy(pbuf, prot_buf, pi_size);
 	}
 
-	setup_iov(iov, buf, pbuf, count, offset);
-	ret = __do_dixio(fd, offset, iocmd, iov, iovcnt);
+	ret = __do_dixio(fd, buf, pbuf, count, offset, rw);
 
 	if (prot_buf && !page_aligned(prot_buf)) {
-		if (ret > 0 && (iocmd == IOCB_CMD_PREADV ||
-				iocmd == IOCB_CMD_PREADVM))
+		if (ret > 0 && (rw == READ))
 			memcpy(prot_buf, pbuf, pi_size);
 
 		gsh_free(pbuf);
@@ -138,17 +174,13 @@ ssize_t do_dixio(int fd, void *buf, void *prot_buf, size_t count, off_t offset,
 ssize_t dixio_pread(int fd, void *buf, void *prot_buf,
                     size_t count, off_t offset)
 {
-	int iocmd = prot_buf ? IOCB_CMD_PREADVM : IOCB_CMD_PREADV;
-
-	return do_dixio(fd, buf, prot_buf, count, offset, iocmd);
+	return do_dixio(fd, buf, prot_buf, count, offset, READ);
 }
 
 
 ssize_t dixio_pwrite(int fd, const void *buf, const void *prot_buf,
                      size_t count, off_t offset)
 {
-	int iocmd = prot_buf ? IOCB_CMD_PWRITEVM : IOCB_CMD_PWRITEV;
-
 	return do_dixio(fd, (void *)buf, (void *)prot_buf, count, offset,
-			iocmd);
+			WRITE);
 }
--- src/FSAL/FSAL_VFS/dixio.h
+++ src/FSAL/FSAL_VFS/dixio.h
@@ -14,14 +14,6 @@
 
 #include "nfs_integrity.h"
 
-#define IOCB_CMD_PREADV		(7)
-#define IOCB_CMD_PWRITEV	(8)
-#define IOCB_CMD_PREADVM	(9)
-#define IOCB_CMD_PWRITEVM	(10)
-
-ssize_t do_dixio(int fd, void *buf, void *prot_buf, size_t count, off_t offset,
-                 int iocmd);
-
 ssize_t dixio_pread(int fd, void *buf, void *prot_buf,
                     size_t count, off_t offset);
 
