44a236a80f23042467312b0c823d0f2efda267b2
Kelong Wang <kelong.wang@stonybrook.edu>
2014-08-16 23:35:10 -0400

    make sure dix buffer is aligned
    might be removed if upper levels already enforce the alignment

--- src/FSAL/FSAL_VFS/dixio.c
+++ src/FSAL/FSAL_VFS/dixio.c
@@ -69,7 +69,7 @@ static void io_prep_extension_pi(struct io_extension *ext, void *buf,
 #endif
 
 static inline size_t page_roundup(size_t size) {
-	return ((size + PAGESIZE - 1) >> PAGESHIFT) * PAGESIZE;
+	return ((size + PAGESIZE - 1) >> PAGESHIFT) << PAGESHIFT;
 }
 
 static inline int page_aligned(void *buf) {
@@ -142,29 +142,53 @@ static ssize_t do_dixio(int fd, void *buf, void *prot_buf, size_t count,
 			off_t offset, int rw)
 {
 	void *pbuf = prot_buf;
+	void *buf_align = buf;
 	ssize_t ret = 0;
 	size_t pi_size = 0;
 
 	if (prot_buf && !page_aligned(prot_buf)) {
 		pi_size = get_pi_size(count);
+		// fprintf(stderr, "pbuf %x not aligned\n", prot_buf);
 		pbuf = gsh_malloc_aligned(PAGESIZE, pi_size);
 		if (!pbuf) {
 			DIX_ERR("could not alloc memory for prot_buf");
 			errno = ENOMEM;
 			return -1;
 		}
-
 		if (rw == WRITE)
 			memcpy(pbuf, prot_buf, pi_size);
 	}
 
-	ret = __do_dixio(fd, buf, pbuf, count, offset, rw);
+	if (!page_aligned(buf)) {
+		// fprintf(stderr, "buf %x not aligned\n", buf);
+		buf_align = gsh_malloc_aligned(PAGESIZE, count);
+		if (!buf_align) {
+			DIX_ERR("could not alloc memory for buf");
+			errno = ENOMEM;
+			return -1;
+		}
+		if (rw == WRITE)
+			memcpy(buf_align, buf, count);
+	}
+
+	fprintf(stderr, "DIX %s %u (%u), %x, %x\n", rw == READ ? "READ" : "WRITE",
+		offset, count, buf_align, pbuf);
+
+	ret = __do_dixio(fd, buf_align, pbuf, count, offset, rw);
 
 	if (prot_buf && !page_aligned(prot_buf)) {
 		if (ret > 0 && (rw == READ))
 			memcpy(prot_buf, pbuf, pi_size);
 
 		gsh_free(pbuf);
+	}
+
+	if (!page_aligned(buf)) {
+		if (ret > 0 && (rw == READ))
+			/* TODO to be examined and removed, if never reach here */
+			memcpy(buf, buf_align, count);
+
+		gsh_free(buf_align);
 	}
 
 	return ret;
