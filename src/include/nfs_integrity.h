e0e3c19489e4172df34943f8b2e43c196e6c4c47
Ming Chen <v.mingchen@gmail.com>
2014-09-17 15:33:20 -0400

    be consistent and reindent the code with tab

--- src/include/nfs_integrity.h
+++ src/include/nfs_integrity.h
@@ -1,5 +1,5 @@
 /*
- * vim:expandtab:shiftwidth=8:tabstop=8:
+ * vim:noexpandtab:shiftwidth=8:tabstop=8:
  *
  * Copyright (C) Stony Brook University 2014
  * by Ming Chen <v.mingchen@gmail.com>
@@ -40,7 +40,7 @@ static inline off_t io_info_to_offset(struct io_info *info) {
 	case NFS4_CONTENT_PROTECT_INFO:
 		return info->io_content.pinfo.pi_offset;
 	default:
-                LogFatal(COMPONENT_FSAL, "unsupported io_content type: %d",
+		LogFatal(COMPONENT_FSAL, "unsupported io_content type: %d",
 			 info->io_content.what);
 		return 0;
 	}
@@ -87,21 +87,21 @@ static inline char* io_info_to_file_data(struct io_info *info) {
 
 static inline void dump_pi_buf(uint8_t *pi_buf, size_t pi_size)
 {
-        char *pi_hex, *curr;
-        int i, hex_len;
-        hex_len = pi_size * 2 + pi_size / 8;
-        pi_hex = gsh_malloc(hex_len);
-        for (i = 0, curr = pi_hex; i < pi_size; i++) {
-                sprintf(curr, "%02x", *(pi_buf + i));
-                curr += 2;
-                if (i % 8 == 7) {
-                        *curr = ' ';
-                        curr += 1;
-                }
-        }
-        *(curr-1) = '\0';
-        LogDebug(COMPONENT_FSAL, "=secnfs=pi_buf: %s", pi_hex);
-        gsh_free(pi_hex);
+	char *pi_hex, *curr;
+	int i, hex_len;
+	hex_len = pi_size * 2 + pi_size / 8;
+	pi_hex = gsh_malloc(hex_len);
+	for (i = 0, curr = pi_hex; i < pi_size; i++) {
+		sprintf(curr, "%02x", *(pi_buf + i));
+		curr += 2;
+		if (i % 8 == 7) {
+			*curr = ' ';
+			curr += 1;
+		}
+	}
+	*(curr - 1) = '\0';
+	LogDebug(COMPONENT_FSAL, "=secnfs=pi_buf: %s", pi_hex);
+	gsh_free(pi_hex);
 }
 
 #endif				/* _NFS_INTEGRITY_H */
