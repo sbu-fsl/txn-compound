822b2df158e82aab7b93f35630377e131634ed08
Ming Chen <v.mingchen@gmail.com>
2015-04-24 16:11:10 -0400

    Script to create, format and mount ramdisk
    Note that this script assumes the ramdisk is /dev/sdb.

---
+++ nfs4int/mount_ramdisk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+umount /vfs-ganesha/
+targetcli restoreconfig pi-10MB-device-config.json clear_existing
+yes | mkfs.ext4 /dev/sdb
+mount -t ext4 /dev/sdb  /vfs-ganesha/
+#tmux -2
+
