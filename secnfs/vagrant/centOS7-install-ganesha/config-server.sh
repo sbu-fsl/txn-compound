8018284f5d73ca579f0bbe36100bf127926fb6a2
Garima Gehlot <garima@crossroads.fsl.cs.sunysb.edu>
2015-07-25 01:50:36 -0400

    Support for Vagrant

--- secnfs/vagrant/centOS7-install-ganesha/config-server.sh
+++ secnfs/vagrant/centOS7-install-ganesha/config-server.sh
@@ -14,7 +14,7 @@ ulimit -H -c 0 --                       # disable core dump
 hash -r                                 # clear the command path hash
 
 # common libraries required
-sudo yum install -y gcc cmake git libtool wget bison flex bc
+sudo yum install -y gcc cmake git libtool wget bison flex bc patch
 
 # parse command line argument for username
 USR="";
@@ -45,18 +45,24 @@ if [ $OPTIND -eq 1 ]; then
         usage
 fi
 
+HOME_DIR=`pwd`;
+
 
 # checkout the integrity-2.1 branch git repository
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+touch ~/.ssh/known_hosts
+ssh-keyscan -H git.fsl.cs.sunysb.edu >> ~/.ssh/known_hosts
 git_repo="$USR@git.fsl.cs.sunysb.edu:/scm/fslgit/fsl-nfs-ganesha.git";
 if [ ! -d fsl-nfs-ganesha ]; then
-  yes | git clone -b integrity-2.1 $git_repo;
+	git clone -b integrity-2.1 $git_repo;
 fi
 
 # checkout the master repository
 sudo mkdir master_repo
 cd master_repo
 if [ ! -d fsl-nfs-ganesha ]; then
-  git clone $git_repo;
+	git clone $git_repo;
 fi
 
 # Download the kernel
@@ -66,7 +72,7 @@ tar -xvf linux-3.14.17.tar.xz
 cd linux-3.14.17
 
 # Apply patches
-for i in ~/fsl-nfs-ganesha/nfs4int/pi-patchset-v2/?-*.patch;
+for i in $HOME_DIR/fsl-nfs-ganesha/nfs4int/pi-patchset-v2/?-*.patch;
 	do
 		if [[ $i != *"0-userspace"* ]]; then
 		sudo patch -p1 < $i;
@@ -74,7 +80,7 @@ for i in ~/fsl-nfs-ganesha/nfs4int/pi-patchset-v2/?-*.patch;
 	done
 
 # Apply fixes
-for i in ~/fsl-nfs-ganesha/nfs4int/pi-patchset-v2/000?-fix*.patch;
+for i in $HOME_DIR/fsl-nfs-ganesha/nfs4int/pi-patchset-v2/000?-fix*.patch;
 	do
 		sudo patch -p1 < $i;
 	done
