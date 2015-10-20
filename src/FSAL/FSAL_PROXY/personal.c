aacde044089393855b756dc14792f78bbc9e8b9f
ASHOKSANKAR HARIHARASUBRAMONY <ashoksankar.h@stonybrook.edu>
2015-10-20 01:25:44 -0400

    lookup, read, readdir, getattr and write working

--- src/FSAL/FSAL_PROXY/personal.c
+++ src/FSAL/FSAL_PROXY/personal.c
@@ -12,12 +12,24 @@
 #include "pxy_fsal_methods.h"
 #include "personal.h"
 
+bool
+readdir_reply(const char *name, void *dir_state,
+                fsal_cookie_t cookie)
+{
+	LogDebug(COMPONENT_FSAL, "readdir_reply() called\n");
+	return true;
+}
+
 int personal_init() {
 	struct pxy_fsal_module *new_module = NULL;
 	struct gsh_export *export = NULL;
 	struct fsal_obj_handle *vfs0_handle = NULL;
+	struct fsal_obj_handle *abcd_handle = NULL;
 	fsal_status_t fsal_status = { 0, 0 };
 	cache_entry_t *entry = NULL;
+	char *data_buf = NULL;
+	size_t read_amount = 0;
+	bool eof = false;
 	struct req_op_context req_ctx;
 
 	LogDebug(COMPONENT_FSAL, "personal_init() called\n");
@@ -50,12 +62,68 @@ int personal_init() {
 	op_ctx->fsal_export = export->fsal_export;
 
 	//fsal_status = pxy_lookup(NULL, "vfs0", &vfs0_handle);
-	fsal_status = export->fsal_export->obj_ops->lookup(NULL, "vfs1", &vfs0_handle);
+	fsal_status = export->fsal_export->obj_ops->lookup(NULL, "vfs0", &vfs0_handle);
 	if (FSAL_IS_ERROR(fsal_status)) {
 		LogDebug(COMPONENT_FSAL, "lookup() for vfs0 failed\n");
+		return -1;
 	}
 
 	LogDebug(COMPONENT_FSAL, "lookup() for vfs0 succeeded\n");
+	if (vfs0_handle == NULL) {
+		LogDebug(COMPONENT_FSAL, "vfs0_handle is NULL\n");
+		return -1;
+	}
+
+	fsal_status = export->fsal_export->obj_ops->lookup(vfs0_handle, "abcd", &abcd_handle);
+	if (FSAL_IS_ERROR(fsal_status)) {
+                LogDebug(COMPONENT_FSAL, "lookup() for abcd failed\n");
+		return -1;
+        }
+
+	LogDebug(COMPONENT_FSAL, "lookup() for abcd succeeded\n");
+	if (abcd_handle == NULL) {
+		LogDebug(COMPONENT_FSAL, "abcd_handle is NULL\n");
+		return -1;
+	}
+
+	fsal_status = export->fsal_export->obj_ops->read(abcd_handle, 0, 1024, data_buf, &read_amount, &eof);
+	if (FSAL_IS_ERROR(fsal_status)) {
+                LogDebug(COMPONENT_FSAL, "read() for abcd failed\n");
+                return -1;
+        }
+
+	LogDebug(COMPONENT_FSAL, "read() for abcd succeeded\n");
+	LogDebug(COMPONENT_FSAL, "amount read: %u\n", read_amount);
+
+	fsal_status = export->fsal_export->obj_ops->getattrs(abcd_handle);
+	if (FSAL_IS_ERROR(fsal_status)) {
+                LogDebug(COMPONENT_FSAL, "getattr() for abcd failed\n");
+                return -1;
+        }
+
+	LogDebug(COMPONENT_FSAL, "getattr() for abcd succeeded\n");
+
+	fsal_status = export->fsal_export->obj_ops->readdir(abcd_handle, NULL, NULL, readdir_reply, &eof);
+	if (FSAL_IS_ERROR(fsal_status)) {
+                LogDebug(COMPONENT_FSAL, "read_dir() for abcd failed\n");
+        }
+
+	fsal_status = export->fsal_export->obj_ops->readdir(vfs0_handle, NULL, NULL, readdir_reply, &eof);
+        if (FSAL_IS_ERROR(fsal_status)) {
+                LogDebug(COMPONENT_FSAL, "read_dir() for vfs0 failed\n");
+                return -1;
+        }
+
+        LogDebug(COMPONENT_FSAL, "readdir() for vfs0 succeeded\n");
+
+	read_amount = 0;
+	fsal_status = export->fsal_export->obj_ops->write(abcd_handle, 0, 20, "check write", &read_amount, &eof);
+        if (FSAL_IS_ERROR(fsal_status)) {
+                LogDebug(COMPONENT_FSAL, "write() for abcd failed\n");
+                return -1;
+        }
+
+	LogDebug(COMPONENT_FSAL, "write() for abcd succeeded, %u written\n", read_amount);
 
 	return 0;
 }
--- src/include/personal.h
+++ src/include/personal.h
@@ -1,4 +1,6 @@
 #include "export_mgr.h"
 
 int personal_init();
-
+bool
+readdir_reply(const char *name, void *dir_state,
+                fsal_cookie_t cookie);
