6c91be262baa41dad99f95f120fa73b59ef7cf6f
ASHOKSANKAR HARIHARASUBRAMONY <ashoksankar.h@stonybrook.edu>
2016-02-19 21:47:20 -0500

    change directory structure - TC_client folder added, FSAL_PROXY removed

---
+++ proxy_client/TC_client/proxy.c
@@ -0,0 +1,214 @@
+/*
+ * vim:noexpandtab:shiftwidth=8:tabstop=8:
+ *
+ * Copyright (C) Max Matveev, 2012
+ *
+ * Copyright CEA/DAM/DIF  (2008)
+ *
+ * contributeur : Philippe DENIEL   philippe.deniel@cea.fr
+ *                Thomas LEIBOVICI  thomas.leibovici@cea.fr
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "config.h"
+
+#include <ctype.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "fsal.h"
+#include "FSAL/fsal_init.h"
+#include "pxy_fsal_methods.h"
+
+/* defined the set of attributes supported with POSIX */
+#define SUPPORTED_ATTRIBUTES (                                       \
+		ATTR_TYPE     | ATTR_SIZE     |			     \
+		ATTR_FSID     | ATTR_FILEID   |			     \
+		ATTR_MODE     | ATTR_NUMLINKS | ATTR_OWNER     |     \
+		ATTR_GROUP    | ATTR_ATIME    | ATTR_RAWDEV    |     \
+		ATTR_CTIME    | ATTR_MTIME    | ATTR_SPACEUSED |     \
+		ATTR_CHGTIME)
+
+/* filesystem info for PROXY */
+static struct fsal_staticfsinfo_t proxy_info = {
+	.maxfilesize = UINT64_MAX,
+	.maxlink = _POSIX_LINK_MAX,
+	.maxnamelen = 1024,
+	.maxpathlen = 1024,
+	.no_trunc = true,
+	.chown_restricted = true,
+	.case_preserving = true,
+	.lock_support = true,
+	.named_attr = true,
+	.unique_handles = true,
+	.lease_time = {10, 0},
+	.acl_support = FSAL_ACLSUPPORT_ALLOW,
+	.homogenous = true,
+	.supported_attrs = SUPPORTED_ATTRIBUTES,
+};
+
+#ifdef _USE_GSSRPC
+static struct config_item_list sec_types[] = {
+	CONFIG_LIST_TOK("krb5", RPCSEC_GSS_SVC_NONE),
+	CONFIG_LIST_TOK("krb5i", RPCSEC_GSS_SVC_INTEGRITY),
+	CONFIG_LIST_TOK("krb5p", RPCSEC_GSS_SVC_PRIVACY),
+	CONFIG_LIST_EOL
+};
+#endif
+
+static struct config_item proxy_client_params[] = {
+	CONF_ITEM_UI32("Retry_SleepTime", 0, 60, 10,
+		       pxy_client_params, retry_sleeptime),
+	CONF_ITEM_IPV4_ADDR("Srv_Addr", "127.0.0.1",
+			    pxy_client_params, srv_addr),
+	CONF_ITEM_UI32("NFS_Service", 0, UINT32_MAX, 100003,
+		       pxy_client_params, srv_prognum),
+	CONF_ITEM_UI32("NFS_SendSize", 512, FSAL_MAXIOSIZE, 32768,
+		       pxy_client_params, srv_sendsize),
+	CONF_ITEM_UI32("NFS_RecvSize", 512, FSAL_MAXIOSIZE, 32768,
+		       pxy_client_params, srv_recvsize),
+	CONF_ITEM_INET_PORT("NFS_Port", 0, UINT16_MAX, 2049,
+			    pxy_client_params, srv_port),
+	CONF_ITEM_BOOL("Use_Privileged_Client_Port", false,
+		       pxy_client_params, use_privileged_client_port),
+	CONF_ITEM_UI32("RPC_Client_Timeout", 1, 60*4, 60,
+		       pxy_client_params, srv_timeout),
+#ifdef _USE_GSSRPC
+	CONF_ITEM_STR("Remote_PrincipalName", 0, MAXNAMLEN, NULL,
+		      pxy_client_params, remote_principal),
+	CONF_ITEM_STR("KeytabPath", 0, MAXPATHLEN, "/etc/krb5.keytab"
+		      pxy_client_params, keytab),
+	CONF_ITEM_UI32("Credential_LifeTime", 0, 86400*2, 86400,
+		       pxy_client_params, cred_lifetime),
+	CONF_ITEM_ENUM("Sec_Type", RPCSEC_GSS_SVC_NONE, sec_types,
+		       pxy_client_params, sec_type),
+	CONF_ITEM_BOOL("Active_krb5", false,
+		       pxy_client_params, active_krb5),
+#endif
+#ifdef PROXY_HANDLE_MAPPING
+	CONF_ITEM_BOOL("Enable_Handle_Mapping", false,
+		       pxy_client_params, enable_handle_mapping),
+	CONF_ITEM_STR("HandleMap_DB_Dir", 0, MAXPATHLEN,
+		      "/var/ganesha/handlemap",
+		      pxy_client_params, hdlmap.databases_directory),
+	CONF_ITEM_STR("HandleMap_Tmp_Dir", 0, MAXPATHLEN,
+		      "/var/ganesha/tmp",
+		      pxy_client_params, hdlmap.temp_directory),
+	CONF_ITEM_UI32("HandleMap_DB_Count", 1, 16, 8,
+		       pxy_client_params, hdlmap.database_count),
+	CONF_ITEM_UI32("HandleMap_HashTable_Size", 1, 127, 103,
+		       pxy_client_params, hdlmap.hashtable_size),
+#endif
+	CONFIG_EOL
+};
+
+
+/**
+ * @brief Validate and commit the proxy params
+ *
+ * This is also pretty simple.  Just a NOP in both cases.
+ *
+ * @param link_mem - pointer to the link_mem struct memory.
+ * @param self_struct - NULL for init parent, not NULL for attaching
+ */
+
+static struct config_item proxy_params[] = {
+	CONF_ITEM_BOOL("link_support", true,
+		       pxy_fsal_module, fsinfo.link_support),
+	CONF_ITEM_BOOL("symlink_support", true,
+		       pxy_fsal_module, fsinfo.symlink_support),
+	CONF_ITEM_BOOL("cansettime", true,
+		       pxy_fsal_module, fsinfo.cansettime),
+	CONF_ITEM_UI64("maxread", 512, FSAL_MAXIOSIZE, FSAL_MAXIOSIZE,
+		       pxy_fsal_module, fsinfo.maxread),
+	CONF_ITEM_UI64("maxwrite", 512, FSAL_MAXIOSIZE, FSAL_MAXIOSIZE,
+		       pxy_fsal_module, fsinfo.maxwrite),
+	CONF_ITEM_MODE("umask", 0, 0777, 0,
+		       pxy_fsal_module, fsinfo.umask),
+	CONF_ITEM_BOOL("auth_xdev_export", false,
+		       pxy_fsal_module, fsinfo.auth_exportpath_xdev),
+	CONF_ITEM_MODE("xattr_access_rights", 0, 0777, 0400,
+		       pxy_fsal_module, fsinfo.xattr_access_rights),
+	CONF_ITEM_BLOCK("remote_server", proxy_client_params,
+			noop_conf_init, noop_conf_commit,
+			pxy_fsal_module, special), /*fake filler */
+	CONFIG_EOL
+};
+
+struct config_block proxy_param = {
+	.dbus_interface_name = "org.ganesha.nfsd.config.fsal.proxy",
+	.blk_desc.name = "PROXY",
+	.blk_desc.type = CONFIG_BLOCK,
+	.blk_desc.u.blk.init = noop_conf_init,
+	.blk_desc.u.blk.params = proxy_params,
+	.blk_desc.u.blk.commit = noop_conf_commit
+};
+
+
+static fsal_status_t pxy_init_config(struct fsal_module *fsal_hdl,
+				     config_file_t config_struct)
+{
+	struct config_error_type err_type;
+	int rc;
+	struct pxy_fsal_module *pxy =
+	    container_of(fsal_hdl, struct pxy_fsal_module, module);
+
+	pxy->fsinfo = proxy_info;
+	(void) load_config_from_parse(config_struct,
+				      &proxy_param,
+				      pxy,
+				      true,
+				      &err_type);
+	if (!config_error_is_harmless(&err_type))
+		return fsalstat(ERR_FSAL_INVAL, 0);
+
+#ifdef PROXY_HANDLE_MAPPING
+	rc = HandleMap_Init(&pxy->special.hdlmap);
+	if (rc < 0)
+		return fsalstat(ERR_FSAL_INVAL, -rc);
+#endif
+
+	rc = pxy_init_rpc(pxy);
+	if (rc)
+		return fsalstat(ERR_FSAL_FAULT, rc);
+	return fsalstat(ERR_FSAL_NO_ERROR, 0);
+}
+
+static struct pxy_fsal_module PROXY;
+
+MODULE_INIT void pxy_init(void)
+{
+	if (register_fsal(&PROXY.module, "PROXY", FSAL_MAJOR_VERSION,
+			  FSAL_MINOR_VERSION, FSAL_ID_NO_PNFS) != 0)
+		return;
+	PROXY.module.ops->init_config = pxy_init_config;
+	PROXY.module.ops->create_export = pxy_create_export;
+}
+
+MODULE_FINI void pxy_unload(void)
+{
+	int retval;
+
+	retval = unregister_fsal(&PROXY.module);
+	if (retval != 0) {
+		fprintf(stderr, "PROXY module failed to unregister");
+		return;
+	}
+}
---
+++ proxy_client/TC_client/test2.c
@@ -0,0 +1,126 @@
+#include "config.h"
+#include <ctype.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "fsal_types.h"
+#include "fsal_api.h"
+#include "fsal.h"
+#include "FSAL/fsal_init.h"
+#include "fs_fsal_methods.h"
+#include "tc_utils.h"
+#include <time.h>
+
+int test2(char *input_path, int block_size, int num_files, int num_ops,
+	  int ops_per_comp, int rw)
+{
+	struct fsal_module *new_module = NULL;
+	struct gsh_export *export = NULL;
+	struct fsal_obj_handle *vfs0_handle = NULL;
+	fsal_status_t fsal_status = { 0, 0 };
+	struct req_op_context req_ctx;
+	struct tc_iovec *user_arg = NULL;
+	struct tc_iovec *cur_arg = NULL;
+	char *temp_path = NULL;
+	char *data_buf = NULL;
+	int  input_len = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	clock_t t;
+	float time_taken;
+	struct timeval tv1, tv2;
+
+	LogDebug(COMPONENT_FSAL, "test2() called\n");
+	new_module = lookup_fsal("PROXY");
+	if (new_module == NULL) {
+		LogDebug(COMPONENT_FSAL, "Proxy Module Not found\n");
+		return -1;
+	}
+	LogDebug(COMPONENT_FSAL, "Proxy Module Found\n");
+	export = get_gsh_export(77);
+	if(export == NULL){
+		LogDebug(COMPONENT_FSAL, "Export Not found\n");
+		return -1;
+	}
+	LogDebug(COMPONENT_FSAL, "Export Found\n");
+	LogDebug(COMPONENT_FSAL,
+                 "Export %d at pseudo (%s) with path (%s) and tag (%s) \n",
+                 export->export_id, export->pseudopath,
+                 export->fullpath, export->FS_tag);
+
+	
+	sleep(1);
+
+	memset(&req_ctx, 0, sizeof(struct req_op_context));
+	op_ctx = &req_ctx;
+	op_ctx->creds = NULL;
+	op_ctx->export = export;
+	op_ctx->fsal_export = export->fsal_export;
+
+	input_len = strlen(input_path);
+	temp_path = malloc(input_len + 4);
+
+	user_arg = malloc(ops_per_comp * (sizeof(struct tc_iovec)));
+	k = 0;
+	while (k < ops_per_comp) {
+		cur_arg = user_arg + k;
+		cur_arg->data = malloc(block_size);
+		k++;
+	}
+
+	//t = clock();
+	gettimeofday(&tv1, NULL);
+
+	while (j < num_files) {
+
+		i = 0;
+		while (i < num_ops / ops_per_comp) {
+
+			k = 0;
+			while (k < ops_per_comp) {
+				cur_arg = user_arg + k;
+				cur_arg->path = NULL;
+				cur_arg->offset =
+				    (i * ops_per_comp * block_size) +
+				    (k * block_size);
+				cur_arg->length = block_size;
+				k++;
+			}
+
+			snprintf(temp_path, input_len + 4, "%s%d", input_path,
+				 j);
+			user_arg->path = temp_path;
+
+			if (rw == 0) {
+				tcread_v(user_arg, ops_per_comp, FALSE);
+			} else {
+				tcwrite_v(user_arg, ops_per_comp, FALSE);
+			}
+			i++;
+		}
+
+		j++;
+	}
+
+	//t = clock() - t;
+	//time_taken = ((double)t)/CLOCKS_PER_SEC;
+	//time_taken = (float) t;
+	gettimeofday(&tv2, NULL);
+	time_taken = ((double)(tv2.tv_usec - tv1.tv_usec) / 1000000) +
+		     (double)(tv2.tv_sec - tv1.tv_sec);
+	LogFatal(COMPONENT_FSAL, "tcreads done - %f seconds\n", time_taken);
+
+	i = 0;
+	while (i < 100) {
+		cur_arg = user_arg + i;
+		free(cur_arg->data);
+		i++;
+	}
+
+	free(user_arg);
+	free(temp_path);
+	return 0;
+}
