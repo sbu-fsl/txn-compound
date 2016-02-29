38bc2873f30917a807a5687c9e598d845c54b5fc
Garima Gehlot <root@dhcp236.fsl.cs.sunysb.edu>
2016-02-29 12:34:52 -0500

    minor changes for file descriptor support and formatted the code

--- proxy_client/posix/tc_impl_posix.c
+++ proxy_client/posix/tc_impl_posix.c
@@ -7,7 +7,7 @@
 
 #include "tc_impl_posix.h"
 
-#define POSIX_WARN(fmt, args...) fprintf(stderr, "==posix-WARN==" fmt, ## args)
+#define POSIX_WARN(fmt, args...) fprintf(stderr, "==posix-WARN==" fmt, ##args)
 
 /*
  * open routine for POSIX files
@@ -19,6 +19,8 @@ tc_file posix_open(const char *path, int flags)
 {
 	tc_file file;
 	int fd = open(path, flags);
+
+	file.type = FILE_DESCRIPTOR;
 	file.fd = fd;
 
 	return file;
@@ -34,10 +36,10 @@ int posix_close(const tc_file *file)
 {
 	int err = 0;
 
-	assert(file->type != FILE_DESCRIPTOR);
+	assert(file->type == FILE_DESCRIPTOR);
 
 	/* return error no in case of failure */
-	if(close(file->fd) < 0)
+	if (close(file->fd) < 0)
 		err = errno;
 
 	return err;
@@ -51,36 +53,37 @@ int posix_close(const tc_file *file)
  */
 tc_res posix_readv(struct tc_iovec *arg, int read_count, bool is_transaction)
 {
-	int fd, amount_read, i=0;
-	tc_file file = {0};
+	int fd, amount_read, i = 0;
+	tc_file file = { 0 };
 	struct tc_iovec *cur_arg = NULL;
 	tc_res result = { .okay = true, .index = -1, .err_no = 0 };
 
 	POSIX_WARN("posix_readv() called \n");
 
 	while (i < read_count) {
-		cur_arg = arg+i;
+		cur_arg = arg + i;
 
 		/*
 		 * if the user specified the path and not file descriptor
 		 * then call open to obtain the file descriptor else
 		 * go ahead with the file descriptor specified by the user
 		 */
-		if(cur_arg->file.type == FILE_PATH)
+		if (cur_arg->file.type == FILE_PATH)
 			file = posix_open(cur_arg->file.path, O_RDONLY);
 		else
 			file = cur_arg->file;
 
 		fd = file.fd;
-		if(fd < 0) {
+		if (fd < 0) {
 			result.okay = false;
 			break;
 		}
 
 		/* Read data */
-		amount_read = pread(fd, cur_arg->data, cur_arg->length, cur_arg->offset);
-		if(amount_read < 0) {
-			if(file.type == FILE_PATH)
+		amount_read =
+		    pread(fd, cur_arg->data, cur_arg->length, cur_arg->offset);
+		if (amount_read < 0) {
+			if (cur_arg->file.type == FILE_PATH)
 				posix_close(&file);
 			result.okay = false;
 			break;
@@ -89,7 +92,7 @@ tc_res posix_readv(struct tc_iovec *arg, int read_count, bool is_transaction)
 		/* set the length to number of bytes successfully read */
 		cur_arg->length = amount_read;
 
-		if(file.type == FILE_PATH && posix_close(&file) < 0) {
+		if (cur_arg->file.type == FILE_PATH && posix_close(&file) < 0) {
 			result.okay = false;
 			break;
 		}
@@ -98,7 +101,7 @@ tc_res posix_readv(struct tc_iovec *arg, int read_count, bool is_transaction)
 	}
 
 	/* No error encountered */
-	if(result.okay)
+	if (result.okay)
 		goto exit;
 
 	result.index = i;
@@ -118,16 +121,16 @@ exit:
  */
 tc_res posix_writev(struct tc_iovec *arg, int write_count, bool is_transaction)
 {
-	int fd, amount_written, i=0;
-	tc_file file = {0};
-        struct tc_iovec *cur_arg = NULL;
+	int fd, amount_written, i = 0;
+	tc_file file = { 0 };
+	struct tc_iovec *cur_arg = NULL;
 	tc_res result = { .okay = true, .index = -1, .err_no = 0 };
 	int flags;
 
-        POSIX_WARN("posix_writev() called \n");
+	POSIX_WARN("posix_writev() called \n");
 
-        while (i < write_count) {
-                cur_arg = arg+i;
+	while (i < write_count) {
+		cur_arg = arg + i;
 
 		/* open the requested file */
 		flags = O_WRONLY;
@@ -135,24 +138,23 @@ tc_res posix_writev(struct tc_iovec *arg, int write_count, bool is_transaction)
 			flags |= O_CREAT;
 		}
 
-		if(cur_arg->file.type == FILE_PATH)
+		if (cur_arg->file.type == FILE_PATH)
 			file = posix_open(cur_arg->file.path, flags);
 		else
 			file = cur_arg->file;
 
 		fd = file.fd;
-		if(fd < 0) {
+		if (fd < 0) {
 			result.okay = false;
 			break;
 		}
 
 		/* Write data */
-		amount_written = pwrite(fd, cur_arg->data, cur_arg->length, cur_arg->offset);
+		amount_written =
+		    pwrite(fd, cur_arg->data, cur_arg->length, cur_arg->offset);
 
-		if(amount_written < 0) {
-			perror("");
-			POSIX_WARN("offset : %d\n", cur_arg->offset);
-			if(file.type == FILE_PATH)
+		if (amount_written < 0) {
+			if (cur_arg->file.type == FILE_PATH)
 				posix_close(&file);
 			result.okay = false;
 			break;
@@ -161,16 +163,16 @@ tc_res posix_writev(struct tc_iovec *arg, int write_count, bool is_transaction)
 		/* set the length to number of bytes successfully written */
 		cur_arg->length = amount_written;
 
-		if(file.type == FILE_PATH && posix_close(&file) < 0) {
+		if (cur_arg->file.type == FILE_PATH && posix_close(&file) < 0) {
 			result.okay = false;
 			break;
 		}
 
-                i++;
-        }
+		i++;
+	}
 
 	/* No errors encountered */
-	if(result.okay)
+	if (result.okay)
 		goto exit;
 
 	/* Set the index at which error occured and the error no */
@@ -192,33 +194,32 @@ exit:
  */
 void copy_attrs(const struct stat *st, struct tc_attrs *attr_obj)
 {
-	if(attr_obj->masks.has_mode)
+	if (attr_obj->masks.has_mode)
 		attr_obj->mode = st->st_mode;
 
-	if(attr_obj->masks.has_size)
+	if (attr_obj->masks.has_size)
 		attr_obj->size = st->st_size;
 
-	if(attr_obj->masks.has_nlink)
+	if (attr_obj->masks.has_nlink)
 		attr_obj->nlink = st->st_nlink;
 
-	if(attr_obj->masks.has_uid)
+	if (attr_obj->masks.has_uid)
 		attr_obj->uid = st->st_uid;
 
-	if(attr_obj->masks.has_gid)
+	if (attr_obj->masks.has_gid)
 		attr_obj->gid = st->st_gid;
 
-	if(attr_obj->masks.has_rdev)
+	if (attr_obj->masks.has_rdev)
 		attr_obj->rdev = st->st_rdev;
 
-	if(attr_obj->masks.has_atime)
+	if (attr_obj->masks.has_atime)
 		attr_obj->atime = st->st_atime;
 
-	if(attr_obj->masks.has_mtime)
+	if (attr_obj->masks.has_mtime)
 		attr_obj->mtime = st->st_mtime;
 
-	if(attr_obj->masks.has_ctime)
+	if (attr_obj->masks.has_ctime)
 		attr_obj->ctime = st->st_ctime;
-
 }
 
 /**
@@ -232,20 +233,22 @@ void copy_attrs(const struct stat *st, struct tc_attrs *attr_obj)
 tc_res posix_getattrsv(struct tc_attrs *attrs, int count, bool is_transaction)
 {
 	int fd = -1, i = 0;
-	struct tc_attrs *cur_attr = attrs;
+	struct tc_attrs *cur_attr = NULL;
 	tc_res result = { .okay = true, .index = -1, .err_no = 0 };
 	struct stat st;
 
 	POSIX_WARN("posix_getattrsv() called \n");
 
-	while(i<count) {
-		if(cur_attr->path != NULL) {
+	while (i < count) {
+		cur_attr = attrs + i;
+
+		if (cur_attr->path != NULL) {
 
 			/* get attributes */
-			if(stat(cur_attr->path, &st)<0) {
+			if (stat(cur_attr->path, &st) < 0) {
 				result.okay = false;
 				result.err_no = errno;
-        			result.index = i;
+				result.index = i;
 				POSIX_WARN(
 				    "posix_getattrsv() failed at index : %d\n",
 				    result.index);
@@ -253,8 +256,7 @@ tc_res posix_getattrsv(struct tc_attrs *attrs, int count, bool is_transaction)
 			}
 
 			/* copy stat output */
-			copy_attrs(&st, cur_attr); 
-
+			copy_attrs(&st, cur_attr);
 		}
 
 		i++;
@@ -263,33 +265,30 @@ tc_res posix_getattrsv(struct tc_attrs *attrs, int count, bool is_transaction)
 	return result;
 }
 
-
 static int helper_set_attrs(struct tc_attrs *attrs)
 {
 	int res = 0;
 	struct stat s;
-        struct timeval times[2] = {};
-
+	struct timeval times[2] = {};
 
 	/* check if nlink bit is set, if set return with error */
-	if(attrs->masks.has_nlink) {
+	if (attrs->masks.has_nlink) {
 		POSIX_WARN("set_attrs() failed : nlink attribute bit"
 			   " should not be set \n");
 		return -1;
 	}
 
 	/* check if rdev bit is set, if set return with error */
-	if(attrs->masks.has_rdev) {
+	if (attrs->masks.has_rdev) {
 		POSIX_WARN("set_attrs() failed : rdev attribute bit"
 			   " should not be set \n");
 		return -1;
 	}
 
-
 	/* set the mode */
-	if(attrs->masks.has_mode) {
+	if (attrs->masks.has_mode) {
 		res = chmod(attrs->path, attrs->mode);
-		if(res < 0) {
+		if (res < 0) {
 			POSIX_WARN("helper_set_attrs() failed in setting"
 				   "attribute 'permissions'  of file %s\n",
 				   attrs->path);
@@ -298,9 +297,9 @@ static int helper_set_attrs(struct tc_attrs *attrs)
 	}
 
 	/* set the file size */
-	if(attrs->masks.has_size){
+	if (attrs->masks.has_size) {
 		res = truncate(attrs->path, attrs->size);
-                if(res < 0) {
+		if (res < 0) {
 			POSIX_WARN("helper_set_attrs() failed in setting"
 				   " attribute size of the file %s \n",
 				   attrs->path);
@@ -309,10 +308,10 @@ static int helper_set_attrs(struct tc_attrs *attrs)
 	}
 
 	/* set the UID and GID */
-	if(attrs->masks.has_uid || attrs->masks.has_gid) {
+	if (attrs->masks.has_uid || attrs->masks.has_gid) {
 		res = chown(attrs->path, attrs->uid, attrs->gid);
 
-		if(res < 0) {
+		if (res < 0) {
 			POSIX_WARN("helper_set_attrs() failed in setting "
 				   "attributes 'UID and GID' of the file %s\n",
 				   attrs->path);
@@ -321,22 +320,21 @@ static int helper_set_attrs(struct tc_attrs *attrs)
 	}
 
 	/* set the atime and mtime */
-	if(attrs->masks.has_atime || attrs->masks.has_mtime) {
+	if (attrs->masks.has_atime || attrs->masks.has_mtime) {
 
-        	stat(attrs->path, &s);
-        	times[0].tv_sec = s.st_atime;
-        	times[1].tv_sec = s.st_mtime;
+		stat(attrs->path, &s);
+		times[0].tv_sec = s.st_atime;
+		times[1].tv_sec = s.st_mtime;
 
-
-		if(attrs->masks.has_atime)
+		if (attrs->masks.has_atime)
 			times[0].tv_sec = attrs->atime;
 
-		if(attrs->masks.has_mtime)
+		if (attrs->masks.has_mtime)
 			times[1].tv_sec = attrs->mtime;
 
-        	res = utimes(attrs->path, times);
+		res = utimes(attrs->path, times);
 
-		if(res < 0) {
+		if (res < 0) {
 			POSIX_WARN(
 			    "helper_set_attrs() failed in setting the "
 			    "attributes 'atime and mtime' of the file %s\n",
@@ -344,7 +342,6 @@ static int helper_set_attrs(struct tc_attrs *attrs)
 
 			goto exit;
 		}
-
 	}
 
 exit:
@@ -361,19 +358,22 @@ exit:
 tc_res posix_setattrsv(struct tc_attrs *attrs, int count, bool is_transaction)
 {
 	int fd = -1, i = 0;
-	struct tc_attrs *cur_attr = attrs;
+	struct tc_attrs *cur_attr = NULL;
 	tc_res result = { .okay = true, .index = -1, .err_no = 0 };
 
 	POSIX_WARN("posix_setattrsv() called \n");
 
-	while(i<count) {
-		if(cur_attr->path != NULL) {
+	while (i < count) {
+		cur_attr = attrs + i;
 
-			/* Set the attributes if corrseponding mask bit is set */
-			if(helper_set_attrs(cur_attr) < 0) {
+		if (cur_attr->path != NULL) {
+
+			/* Set the attributes if corrseponding mask bit is set
+			 */
+			if (helper_set_attrs(cur_attr) < 0) {
 				result.okay = false;
 				result.err_no = errno;
-        			result.index = i;
+				result.index = i;
 				POSIX_WARN(
 				    "posix_setattrsv() failed at index : %d\n",
 				    result.index);
