9064dc8a04f493a53f8593a59429d75be4f73a48
ASHOKSANKAR HARIHARASUBRAMONY <ashoksankar.h@stonybrook.edu>
2016-03-17 23:38:18 -0400

    code cleanup, change proxy_client to tc_client

---
+++ tc_client/run_script.sh
@@ -0,0 +1,208 @@
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 4194303 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 4194303 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 4194303 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 4194303 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 2097151 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 2097151 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 2097151 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 2097151 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 1048575 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 1048575 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 1048575 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 1048575 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 524287 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 524287 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 524287 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 524287 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 262143 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 262143 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 262143 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 262143 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 131071 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 131071 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 131071 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 131071 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 65535 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 65535 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 65535 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 65535 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 32767 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 32767 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 32767 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 32768 -n 1 -m 32767 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 16384 -n 1 -m 16383 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 16384 -n 1 -m 16383 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 16384 -n 1 -m 16383 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 16384 -n 1 -m 16383 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 8192 -n 1 -m 8191 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 8192 -n 1 -m 8191 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 8192 -n 1 -m 8191 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 8192 -n 1 -m 8191 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 4096 -n 1 -m 4095 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 4096 -n 1 -m 4095 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 4096 -n 1 -m 4095 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 4096 -n 1 -m 4095 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 2048 -n 1 -m 2047 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 2048 -n 1 -m 2047 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 2048 -n 1 -m 2047 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 2048 -n 1 -m 2047 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 1024 -n 1 -m 1023 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 1024 -n 1 -m 1023 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 1024 -n 1 -m 1023 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
+umount /mnt/test
+mount -o vers=4 130.245.126.183:/vfs0 /mnt/test
+./test6 -b 1024 -n 1 -m 1023 -c 5 -l "/mnt/test/test_cdist/abcd" -z 0.0 -w
+
