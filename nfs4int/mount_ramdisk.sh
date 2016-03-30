765212cef7ef8aba14e8bcce135ca37e51220f86
Ming Chen <v.mingchen@gmail.com>
2016-03-30 17:13:24 -0400

    remove files in nfs4int

--- nfs4int/mount_ramdisk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-umount /vfs-ganesha/
-targetcli restoreconfig pi-10GB-device-config.json clear_existing
-yes | mkfs.ext4 /dev/sdb
-mount -t ext4 /dev/sdb  /vfs-ganesha/
-#tmux -2
-
