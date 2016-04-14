de9969aa416359009ed47a293c4918396e13550d
Ming Chen <v.mingchen@gmail.com>
2016-04-14 16:34:06 -0400

    add more tests of common_types.h
    Signed-off-by: Ming Chen <v.mingchen@gmail.com>

--- tc_client/util/common_types_test.cpp
+++ tc_client/util/common_types_test.cpp
@@ -42,8 +42,8 @@ TEST(BufTest, Basics) {
 	EXPECT_EQ(5, buf_append_str(pbuf, "pqrst"));
 	EXPECT_EQ(20, pbuf->size);
 	EXPECT_EQ(-1, buf_append_str(pbuf, "u"));
-	del_buf(pbuf);
 	EXPECT_EQ(0, strncmp("abcdefghijklmnopqrstu", pbuf->data, pbuf->size));
+	del_buf(pbuf);
 }
 
 TEST(BufTest, AutoBuf) {
@@ -53,4 +53,16 @@ TEST(BufTest, AutoBuf) {
 	EXPECT_EQ(5, buf_append_str(abuf, "abcde"));
 	EXPECT_EQ(-1, buf_append_str(abuf, "x"));
 	EXPECT_EQ(5, abuf->size);
+
+	slice_t sl = asslice(abuf);
+	EXPECT_EQ(5, sl.size);
+	EXPECT_EQ(0, strncmp(sl.data, "abcde", sl.size));
+}
+
+TEST(SliceTest, Basics) {
+	const char *msg = "foo-bar";
+	slice_t s1 = mkslice(msg, strlen(msg));
+	slice_t s2 = toslice(msg);
+	EXPECT_EQ(s1.size, s2.size);
+	EXPECT_EQ(0, strcmp(s1.data, s2.data));
 }
