b28e79a8e495b5e4d980603d59b8cb5c0b2cb852
Henry Nelson <hcnelson99@gmail.com>
2016-08-05 14:04:11 -0400

    Batch calls to nfs4_symlinkv() by 8
    Prevents bugs due to hitting compound size limit with too many paths
    passed to tc_symlinkv()

--- tc_client/tc/tc_impl.c
+++ tc_client/tc/tc_impl.c
@@ -915,7 +915,13 @@ tc_res tc_symlinkv(const char **oldpaths, const char **newpaths, int count,
 
 	TC_START_COUNTER(symlink);
 	if (TC_IMPL_IS_NFS4) {
-		tcres = nfs4_symlinkv(oldpaths, newpaths, count, istxn);
+		for (int i = 0; i < count; i += 8) {
+			tcres = nfs4_symlinkv(&oldpaths[i], &newpaths[i], MIN(8, count - i), istxn);
+			if (!tc_okay(tcres)) {
+				tcres.index += i;
+				return tcres;
+			}
+		}
 	} else {
 		tcres = posix_symlinkv(oldpaths, newpaths, count, istxn);
 	}
